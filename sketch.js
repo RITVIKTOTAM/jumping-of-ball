5ebf510403be350cc70b3281471bc52896ccd6fe
RITVIKTOTAM <74972856+RITVIKTOTAM@users.noreply.github.com>
2020-12-22 17:50:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,35 +2,38 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
 
-function preload()
-{
-	
-}
+var engine,world;
+var paperObject1,log1,log2,log3,log4;
+
 
 function setup() {
-	createCanvas(800, 700);
+	var canvas = createCanvas(800,400);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	//Create the Bodies Here.
-
-
-	Engine.run(engine);
-  
+	paperObject1 = new Paper(200,295,20,20,PI/-6);
+    log1 = new  Log(600,385,100,10);
+    log2 = new Ground(400,395,800,10,PI/-2);
+    log3 = new Log(550,365,10,50);
+    log4 = new Log(650,365,10,50);
+    
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  
-  drawSprites();
+  Engine.update(engine);
  
+
+ 
+ paperObject1.display();
+ log1.display();
+ log2.display();
+ log3.display();
+ log4.display();
+ drawSprites();
 }
-
-
-
