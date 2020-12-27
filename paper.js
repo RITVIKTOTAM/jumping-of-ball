6876cd675ffde2051a48d5e6d34b62775f141d5f
RITVIKTOTAM <74972856+RITVIKTOTAM@users.noreply.github.com>
2020-12-27 11:59:36 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,32 +1,20 @@
-class Paper{
-    constructor(x, y, width, height,angle) {
-     
-        var options = {
-          'isStatic':false,
+class Paper {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic:false,
           'restitution':0.3,
           'friction':0.5,
           'density':1.2
       }
-      
-      
-      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.body = Bodies.circle(x,y,width,options);
       this.width = width;
-      this.height = height;
-    Matter.Body.setAngle(this.body,angle)
       World.add(world, this.body);
-      
     }
     display(){
       var pos =this.body.position;
       var angle = this.body.angle;
       push();
       translate(pos.x, pos.y);
-      
-      rotate(angle);
-      fill(255);
-      circle(0, 0, this.width);
       pop();
     }
   };
-  
-
